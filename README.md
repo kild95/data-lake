577a10748ff00ce16bc822f7b6fd9fffcbeab085
Kil <kilian.ocarroll@gmail.com>
2022-10-19 02:33:55 +0000

    First Commit: Works on small dataset

---
+++ README.md
@@ -0,0 +1,45 @@
+# Data Lake with Spark
+
+## Overview
+A music streaming startup, Sparkify, has grown their user base and song database even more and want to move their data warehouse to a data lake. Their data resides in S3, in a directory of JSON logs on user activity on the app, as well as a directory with JSON metadata on the songs in their app.
+
+As their data engineer, you are tasked with building an ETL pipeline that extracts their data from S3, processes them using Spark, and loads the data back into S3 as a set of dimensional tables. This will allow their analytics team to continue finding insights in what songs their users are listening to.
+
+
+## Project Structure
+
+The repository is made up of the following files:
+
+```
+.
+├── dl.cfg       # Configuration file containing AWS IAM credentials
+├── etl.py       # Extracts data from S3 and processes using Spark
+└── README.md
+
+```
+
+## Data Lake Schema
+This project implements a star schema. `songplays` is the fact table in the data model, while `users`, `songs`, `artists`, and `time` are all dimensional tables.
+
+### Fact Table
+* **`songplays`** - records in event data associated with song plays (records with page = NextSong)
+  * `start_time`, `userId`, `level`, `sessionId`, `location`, `userAgent`, `song_id`, `artist_id`, `songplay_id`
+
+### Dimensional Tables
+* **users** - users of the Sparkify app.
+  * `firstName`, `lastName`, `gender`, `level`, `userId`
+* **songs** - collection of songs.
+  * `song_id`, `title`, `artist_id`, `year`, `duration` 
+* **artists** - information about artists.
+  * `artist_id`, `artist_name`, `artist_location`, `artist_lattitude`, `artist_longitude`
+* **time** - timestamps of records in songplays, deconstructed into various date-time parts.
+  * `start_time`, `hour`, `day`, `week`, `month`, `year`, `weekday`
+
+
+## How to Run
+1. Add appropriate AWS IAM Credentials in `dl.cfg`
+2. Specify desired output data path in the `main` function of `etl.py`
+3. Run `etl.py`
+
+## Extra Notes
+When creating the spark session with the code provided by Udacity, I was getting a bad gateway due to `dl.bintray.com` being inaccessible. I had to download the  [aws](https://mvnrepository.com/artifact/org.apache.hadoop/hadoop-aws/2.7.0) jar and rewrite some of the code. More info can be found [here](https://knowledge.udacity.com/questions/886926). For this reason, the aws jar is included in this repo.
---
+++ etl.py
@@ -0,0 +1,155 @@
+import configparser
+from datetime import datetime
+import os
+from pyspark.sql import SparkSession
+# from pyspark.sql.functions import udf, col
+# from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, date_format
+import pyspark.sql.functions as F
+import pyspark.sql.types as t
+from pyspark.conf import SparkConf ## adding this myself - may not need it
+
+config = configparser.ConfigParser()
+config.read('dl.cfg')
+
+os.environ['AWS_ACCESS_KEY_ID']=config['AWS']['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS']['AWS_SECRET_ACCESS_KEY']
+
+conf = SparkConf().set("spark.jars", "/org.apache.hadoop:hadoop-aws:2.7.0.jar") # just trying this per https://knowledge.udacity.com/questions/886926
+
+def create_spark_session():
+    # updating per: https://knowledge.udacity.com/questions/886926 - I downloaded hadoop-aws-2.7.0.jar and placed in the workspace
+    # older config line : # .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:2.7.0") \
+#     spark = SparkSession \
+#         .builder \
+#         .config("spark.jars", "org.apache.hadoop:hadoop-aws:2.7.0") \
+#         .getOrCreate()
+    spark = SparkSession \
+        .builder \
+        .getOrCreate()
+    return spark
+
+
+def process_song_data(spark, input_data, output_data):
+    # get filepath to song data file
+    # unzipped from terminal because spark cannot read zipped files - https://knowledge.udacity.com/questions/145486
+    song_data = f'{input_data}song_data/*/*/*/*.json' 
+    
+    # read song data file
+    df = spark.read.json(song_data)
+
+    # extract columns to create songs table
+    songs_table = df.select(
+            'song_id', 'title', 'artist_id', 'year', 'duration'
+    ).dropDuplicates() # in case there are repeat songs
+    
+    # write songs table to parquet files partitioned by year and artist
+    # using artist_id instead of artist in case there are two artists with the same name
+    songs_table.write.partitionBy('year', 'artist_id').parquet(f'{output_data}songs_table')
+
+#     # extract columns to create artists table
+    artists_table = df.select(
+        'artist_id', 
+        F.col('artist_name').alias('name'), 
+        F.col('artist_location').alias('location'), 
+        F.col('artist_latitude').alias('latitude'), 
+        F.col('artist_longitude').alias('longitude')
+    ).dropDuplicates() # in case there are repeat artists
+    
+#     # write artists table to parquet files
+    artists_table.write.parquet(f'{output_data}artists_table')
+
+
+def process_log_data(spark, input_data, output_data):
+    # get filepath to log data file
+    log_data = f'{input_data}log_data/*.json'
+
+    # read log data file
+    df = spark.read.json(log_data)
+    
+    # filter by actions for song plays
+    df = df.where(F.col('page') == 'NextSong').cache()
+
+    # extract columns for users table    
+    users_table = df.select(
+        F.col('userId').alias('user_id'), 
+        F.col('firstName').alias('first_name'), 
+        F.col('lastName').alias('last_name'), 
+        'gender', 
+        'level'
+    )
+    
+    # write users table to parquet files
+    users_table.write.parquet(f'{output_data}users_table')
+
+    # create timestamp column from original timestamp column ## taking this to mean convert to seconds from ms
+#     get_timestamp = udf() ## I do not want to use a udf if possible
+    # just changing to seconds. I don't see what else would be useful here
+    df = df.withColumn('timestamp', (F.col('ts')/1000)) ## converting to s from ms, as .from_unixtime() works with seconds
+    
+    # create datetime column from original timestamp column  ## taking this to mean get date from seconds
+#     get_datetime = udf() ## I do not want to use a udf if possible
+    df = df.withColumn('datetime', F.from_unixtime(F.col('timestamp')))
+    
+    # extract columns to create time table
+    time_table = df.select(F.col('datetime').alias('start_time')) \
+                        .withColumn('hour', F.hour(F.col('start_time'))) \
+                        .withColumn('day', F.dayofmonth(F.col('start_time'))) \
+                        .withColumn('week', F.weekofyear(F.col('start_time'))) \
+                        .withColumn('month', F.month(F.col('start_time'))) \
+                        .withColumn('year', F.year(F.col('start_time'))) \
+                        .withColumn('weekday', F.dayofweek(F.col('start_time'))) 
+    
+    # write time table to parquet files partitioned by year and month
+    time_table.write.partitionBy('year', 'month').parquet(output_data + 'time_table')
+
+    # read in song data to use for songplays table
+    song_df = spark.read.json(f'{input_data}song_data/*/*/*/*.json')
+
+    # extract columns from joined song and log datasets to create songplays table 
+    # need to match records based on song AND artist
+    songplays_table = df.join(song_df,
+                              on = (df.artist == song_df.artist_name) & (df.song == song_df.title), # song filter alternative: & (df.length == song_df.duration) 
+                              how = 'inner'
+                             ).withColumn('songplay_id', F.monotonically_increasing_id()) \
+    .select(
+        'songplay_id',
+        df.datetime.alias('start_time'), # not sure if I should be using a different time here, but opting for consistency with time_table for now
+        df.userId.alias('user_id'),
+        df.level,
+        song_df.song_id,
+        song_df.artist_id,
+        df.sessionId.alias('session_id'),
+        df.location,
+        df.userAgent.alias('user_agent'),
+    ).withColumn('month', F.month(F.col('start_time'))) \
+                        .withColumn('year', F.year(F.col('start_time'))) \
+    
+    # returns one non-null record on small dataset which is correct based on https://knowledge.udacity.com/questions/215393
+
+    # write songplays table to parquet files partitioned by year and month
+    songplays_table.write.partitionBy('year', 'month').parquet(f'{output_data}songplays_table')
+
+def main():
+    spark = create_spark_session()
+    input_data = "/home/workspace/data/" # "s3a://udacity-dend/" # "s3a://udacity-dend/song-data/"
+    output_data = "/home/workspace/data/" # "s3a://udacity-dend/"
+    
+    process_song_data(spark, input_data, output_data)   
+    process_log_data(spark, input_data, output_data)
+
+
+if __name__ == "__main__":
+    main()
+    
+    
+#     df_temp = spark.read.json('/home/workspace/data/log_data/*.json')
+#     df_temp = df_temp.withColumn('timestamp', (F.col('ts')/1000)) ## converting to s from ms, as from_unixtime() works with seconds
+#     df_temp = df_temp.withColumn('datetime', F.from_unixtime(F.col('timestamp')))
+#     df_temp = df_temp.withColumn('hour', F.hour(F.col('datetime'))) \
+#                         .withColumn('day', F.dayofmonth(F.col('datetime'))) \
+#                         .withColumn('week', F.weekofyear(F.col('datetime'))) \
+#                         .withColumn('month', F.month(F.col('datetime'))) \
+#                         .withColumn('year', F.year(F.col('datetime'))) \
+#                         .withColumn('weekday', F.dayofweek(F.col('datetime'))) 
+#     df_temp.printSchema()
+#     df_temp.select('ts', 'timestamp', 'datetime', 'hour', 'day', 'week', 'month', 'year').show(truncate=False) 
